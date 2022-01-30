c1d1a69a667d9c8b4e6d8228158f2fe40d5cced4
Aleksandr Smirnov <winst500@gmail.com>
2022-01-31 05:33:33 +0700

    add gettig data

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { setChannels } from '../slices/channelsSlice.js';
+import { setMessages } from '../slices/messagesSlice.js';
+import routes from '../routes.js';
 
-const Chat = () => (<h1>Main page</h1>);
+const Chat = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        routes.dataPath(),
+        { headers: { Authorization: `Bearer ${user.token}` } },
+      );
+      dispatch(setChannels(data.channels));
+      dispatch(setMessages(data.messages));
+      console.log(data);
+    };
+    fetchData();
+  });
+
+  return (
+    <h1>Main page</h1>
+  );
+};
 
 export default Chat;
