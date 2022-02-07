2fdcffdb73ba5bc8ccdce6244892765544c2ca01
Aleksandr Smirnov <winst500@gmail.com>
2022-02-07 12:03:08 +0700

    add Rollbar

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import * as Rollbar from '@rollbar/react';
 import { AuthProvider } from './contexts/authContext.jsx';
 import store from './slices/index.js';
 import '../assets/application.scss';
 import './i18n.js';
 import App from './App.jsx';
 
+const rollbarConfig = {
+  accessToken: '26e49ddc069544f38195dde6d96d2ac0',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'production',
+  },
+};
+
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
       <AuthProvider>
-        <App />
+        <Rollbar.Provider config={rollbarConfig}>
+          <Rollbar.ErrorBoundary>
+            <App />
+          </Rollbar.ErrorBoundary>
+        </Rollbar.Provider>
       </AuthProvider>
     </Provider>
   </BrowserRouter>,
