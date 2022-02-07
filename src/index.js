a286155377ec35d15989dbb75ce3ecc8d6b8a520
Aleksandr Smirnov <winst500@gmail.com>
2022-02-07 18:40:43 +0700

    fix render

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import ReactDOM from 'react-dom';
 import socket from './socket.js';
 import init from './init.jsx';
 
-const runApp = () => {
-  const vdom = init(socket);
+const runApp = async () => {
+  const vdom = await init(socket);
   const container = document.getElementById('chat');
   ReactDOM.render(vdom, container);
 };
--- src/init.jsx
+++ src/init.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import { AuthProvider } from './contexts/authContext.jsx';
 import SocketContext from './contexts/socketContext.js';
 import store from './slices/index.js';
 import '../assets/application.scss';
-import './i18n.js';
 import App from './App.jsx';
+import ru from './locales/ru.js';
 
 const rollbarConfig = {
   accessToken: '26e49ddc069544f38195dde6d96d2ac0',
@@ -19,20 +21,32 @@ const rollbarConfig = {
   },
 };
 
-const init = (socket) => (
-  <RollbarProvider config={rollbarConfig}>
-    <ErrorBoundary>
-      <Provider store={store}>
-        <BrowserRouter>
-          <AuthProvider>
-            <SocketContext.Provider value={socket}>
-              <App />
-            </SocketContext.Provider>
-          </AuthProvider>
-        </BrowserRouter>
-      </Provider>
-    </ErrorBoundary>
-  </RollbarProvider>
-);
+const init = async (socket) => {
+  await i18n
+    .use(initReactI18next)
+    .init({
+      lng: 'ru',
+      debug: false,
+      resources: {
+        ru,
+      },
+    });
+
+  return (
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <BrowserRouter>
+            <AuthProvider>
+              <SocketContext.Provider value={socket}>
+                <App />
+              </SocketContext.Provider>
+            </AuthProvider>
+          </BrowserRouter>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
+  );
+};
 
 export default init;
