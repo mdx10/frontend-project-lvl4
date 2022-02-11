855f440c843a9c07a879b310fe7a1475c79348e3
Aleksandr Smirnov <winst500@gmail.com>
2022-02-11 14:20:39 +0700

    fix message form

--- README.md
+++ README.md
@@ -4,6 +4,15 @@
 [![Actions Status](https://github.com/mdx10/frontend-project-lvl4/workflows/Node%20CI/badge.svg)](https://github.com/mdx10/frontend-project-lvl4/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/c8b34dd101d06eb759de/maintainability)](https://codeclimate.com/github/mdx10/frontend-project-lvl4/maintainability)
 
+## Demo
+
+[Click](https://stormy-taiga-14657.herokuapp.com/)
+
+```sh
+login: admin
+pass: admin
+```
+
 ## Requirements
 
 * [Heroku CLI](https://devcenter.heroku.com/articles/heroku-cli)
@@ -21,5 +30,3 @@ $ make start
 # open http://localhost:5000
 ```
 
-
-[Demo](https://stormy-taiga-14657.herokuapp.com/)
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Form, InputGroup } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { Form, InputGroup, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { useFormik } from 'formik';
 import filter from 'leo-profanity';
 import { addMessage } from '../slices/messagesSlice.js';
 import useAuth from '../hooks/useAuth.js';
@@ -10,7 +11,6 @@ import useSocket from '../hooks/useSocket.js';
 const Messages = () => {
   const { t } = useTranslation();
   const { user } = useAuth();
-  const [text, setText] = useState('');
   const dispatch = useDispatch();
   const socket = useSocket();
 
@@ -31,23 +31,30 @@ const Messages = () => {
 
     socket.on('newMessage', (message) => {
       dispatch(addMessage(message));
-      console.log(message);
     });
     return () => socket.removeAllListeners('newMessage');
   }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (text.trim()) {
+  const f = useFormik({
+    initialValues: {
+      body: '',
+    },
+    onSubmit: ({ body }, { resetForm }) => {
       const message = {
-        body: filter.clean(text),
+        body: filter.clean(body),
         channelId: currentChannelId,
         username: user.username,
       };
-      socket.emit('newMessage', message);
-      setText('');
-    }
-  };
+
+      socket.emit('newMessage', message, (res) => {
+        if (res.status === 'ok') {
+          resetForm();
+          return;
+        }
+        console.error('Message was not sent');
+      });
+    },
+  });
 
   return (
     <div className="d-flex flex-column h-100">
@@ -73,23 +80,25 @@ const Messages = () => {
         ))}
       </div>
       <div className="mt-auto px-5 py-3">
-        <Form onSubmit={handleSubmit} className="py-1 border rounded-2" autoComplete="off">
+        <Form onSubmit={f.handleSubmit} className="py-1 border rounded-2" autoComplete="off">
           <InputGroup hasValidation>
             <Form.Control
-              value={text}
-              onChange={(e) => setText(e.target.value)}
+              value={f.values.body}
+              onChange={f.handleChange}
+              onBlur={f.handleBlur}
+              disabled={f.isSubmitting}
               className="border-0 p-0 ps-2"
               name="body"
               type="text"
               placeholder={t('chat.form.body')}
               aria-label={t('chat.form.label')}
             />
-            <button type="submit" className="btn btn-group-vertical" disabled="">
+            <Button type="submit" className="btn btn-group-vertical" variant="" disabled={!f.values.body.trim() || f.isSubmitting}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
                 <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
               </svg>
               <span className="visually-hidden">{t('chat.form.button')}</span>
-            </button>
+            </Button>
           </InputGroup>
         </Form>
       </div>
