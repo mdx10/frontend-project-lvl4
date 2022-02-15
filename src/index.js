fc0639ad7af742ed13b7ca8922bc027a9b5ab401
Aleksandr Smirnov <winst500@gmail.com>
2022-02-15 15:04:54 +0700

    fix socket init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ReactDOM from 'react-dom';
-import socket from './socket.js';
+import { io } from 'socket.io-client';
 import init from './init.jsx';
 
 const runApp = async () => {
+  const socket = io.connect();
   const vdom = await init(socket);
   const container = document.getElementById('chat');
   ReactDOM.render(vdom, container);
